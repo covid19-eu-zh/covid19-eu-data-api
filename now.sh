df297c62888ab0934c5cfa535222df99529d6a46
SherryH <enjoy.wind7rain@gmail.com>
2020-04-16 12:37:59 +0800

    added now.sh

---
+++ now.sh
@@ -0,0 +1,14 @@
+  
+{
+    "name": "covid19-eu-data-api",
+    "version": 1,
+    "routes": [
+        { "src": "/api",
+            "dest": "/api/hello.js",
+        },
+
+      ],
+      "github": {
+        "silent": true
+      }
+}
