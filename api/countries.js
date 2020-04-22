d28357906255f0cefebb62d3c5dc69eda4caa5ec
SherryH <enjoy.wind7rain@gmail.com>
2020-04-22 12:07:06 +0800

    Test to check req shape

--- api/countries.js
+++ api/countries.js
@@ -2,10 +2,19 @@ module.exports = (req, res) => {
 
   const alpha2 = req.params.alpha2
   const dataPath = `./db/${alpha2}.json`
-  fs.readFile(dataPath, 'utf8', (err, data) => {
-    if (err) {
-      throw err
-    }
-    res.status(200).send(JSON.parse(data))
+  res.status(200).json({
+    body: req.body,
+    query: req.query,
+    cookies: req.cookies,
+    param: req.param
+  })
+  // fs.readFile(dataPath, 'utf8', (err, data) => {
+  //   if (err) {
+  //     if (err) {
+  //       res.status(503).send('Unable to scan directory: ' + err)
+  //     }
+  //     throw err
+  //   }
+  //   res.status(200).send(JSON.parse(data))
   })
 }
