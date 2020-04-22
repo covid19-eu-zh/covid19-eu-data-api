31787cd7a2eaf2142e1a321c65b5897397e93023
SherryH <enjoy.wind7rain@gmail.com>
2020-04-22 12:45:27 +0800

    Change req.params to req.query

--- api/countries.js
+++ api/countries.js
@@ -1,20 +1,23 @@
+// req.params doesn't get read in Zeit, so use req.query instead
+// res.status(200).send({
+//   body: req.body,
+//   query: req.query,
+//   cookies: req.cookies,
+//   params: req.params
+// })
+
 module.exports = (req, res) => {
 
-  // const alpha2 = req.params.alpha2
-  // const dataPath = `./db/${alpha2}.json`
-  res.status(200).send({
-    body: req.body,
-    query: req.query,
-    cookies: req.cookies,
-    params: req.params
+  const alpha2 = req.query.alpha2
+  const dataPath = `./db/${alpha2}.json`
+
+  fs.readFile(dataPath, 'utf8', (err, data) => {
+    if (err) {
+      if (err) {
+        res.status(503).send('Unable to scan directory: ' + err)
+      }
+      throw err
+    }
+    res.status(200).send(JSON.parse(data))
   })
-  // fs.readFile(dataPath, 'utf8', (err, data) => {
-  //   if (err) {
-  //     if (err) {
-  //       res.status(503).send('Unable to scan directory: ' + err)
-  //     }
-  //     throw err
-  //   }
-  //   res.status(200).send(JSON.parse(data))
-  // })
 }
