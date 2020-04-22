e1534e785c5f89416963e4a56ca8eb7e59983c6b
SherryH <enjoy.wind7rain@gmail.com>
2020-04-22 12:18:09 +0800

    test again

--- api/countries.js
+++ api/countries.js
@@ -2,7 +2,7 @@ module.exports = (req, res) => {
 
   // const alpha2 = req.params.alpha2
   // const dataPath = `./db/${alpha2}.json`
-  res.status(200).json({
+  res.status(200).send({
     body: req.body,
     query: req.query,
     cookies: req.cookies,
